5962cccaae580be97c6acaa094b5536552f69afb
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-07-30 22:06:23 +0800

    feat(lab01): complete exercise 2 - pointers

--- lab01/ex2_pointers_and_functions.c
+++ lab01/ex2_pointers_and_functions.c
@@ -4,16 +4,16 @@ void add_one(int input) {
   input += 1;
 }
 
-// TODO: create a pointer to input
-void add_one_pointer(______ input) {
-  // TODO: add one to the integer that input points to
-  ________ += 1;
+// DONE: create a pointer to input
+void add_one_pointer(int* input) {
+  // DONE: add one to the integer that input points to
+  *input += 1;
 }
 
-// TODO: create a pointer to a pointer to input
-void add_one_double_ptr(______ input) {
-  // TODO: add one to the integer that input doubly points to
-  ________ += 1;
+// DONE: create a pointer to a pointer to input
+void add_one_double_ptr(int** input) {
+  // DONE: add one to the integer that input doubly points to
+  **input += 1;
 }
 
 int main() {
@@ -29,19 +29,19 @@ int main() {
 
   // Let's try using add_one_pointer
 
-  // TODO: use add_one_pointer to increment x
+  // DONE: use add_one_pointer to increment x
   // Hint: compare the type of x with the type of the argument
   //       for add_one_pointer
-  add_one_pointer(______);
+  add_one_pointer(&x);
 
   // This line should print 6
   printf("add_one_pointer: %d\n", x);
 
-  // TODO: Let's save the pointer to x in y
-  ______ y = ______;
+  // DONE: Let's save the pointer to x in y
+  int* y = &x;
 
-  // TODO: use add_one_double_ptr to increment x again, using the pointer we just made
-  add_one_double_ptr(______);
+  // DONE: use add_one_double_ptr to increment x again, using the pointer we just made
+  add_one_double_ptr(&y);
 
   // This line should print 7
   printf("add_one_double_ptr: %d\n", x);
