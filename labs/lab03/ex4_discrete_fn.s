97f5f11c16d66745a7d0a1d4d337ff5d5d5f5c33
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-08-04 14:03:31 +0800

    feat(lab03): complete exercise 4

--- labs/lab03/ex4_discrete_fn.s
+++ labs/lab03/ex4_discrete_fn.s
@@ -6,6 +6,13 @@
 # The return value should be stored in a0
 f:
     # Your code here
+   
+    addi t0, a0, 3 # convert the inputs [-3, 3] to indices [0, 6]
+    slli t0, t0, 2 # t0 *= 4; now the offset in bytes
+    add  t0, t0, a1 # add the base address of the output array to the offset
+    lw a0, 0(t0) # load the value at the offset into a0
+    # now a0 contains the value of f at the input value
+    # we can return from the function
 
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
